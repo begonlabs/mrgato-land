7eaaacf01deb07658979189e60506223f0fe6df1
walkercito <yoanky2005@gmail.com>
2025-05-15 17:31:20 -0400

    add: Clerk auth working

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from "@clerk/astro/server";
+
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
-
 export const onRequest = clerkMiddleware((auth, context) => {
   const { redirectToSignIn, userId } = auth();
 
   if (!userId && isProtectedRoute(context.request)) {
-
     return redirectToSignIn();
   }
-});
+}); 

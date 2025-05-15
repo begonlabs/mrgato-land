6af842b3d700993b0a4b2f80c2664889dceda823
walkercito <yoanky2005@gmail.com>
2025-05-15 16:28:37 -0400

    deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import clerk from "@clerk/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
+

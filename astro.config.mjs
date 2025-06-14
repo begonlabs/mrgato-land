905b7368d8158d0ed43d11545cfdbf357c176564
Walkercito <yoanky2005@gmail.com>
2025-06-14 16:55:19 +0000

    vercel rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import clerk from "@clerk/astro";
-import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk()],
-  adapter: node({ mode: "standalone" }),
-  output: "server",
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "server",
+  adapter: vercel(),
 });
-

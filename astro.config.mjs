f426e94c3cf79c8d703c774eee499a81d6e3b62d
Walkercito <yoanky2005@gmail.com>
2025-06-14 16:58:27 +0000

    lock

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 import clerk from "@clerk/astro";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
